59c15123a074d9f115e0cac478de733acfe1ce3b
Letrico <15125831+Letrico@users.noreply.github.com>
2025-05-17 00:27:56 +1000

    alfred plugin stuff

--- tasks/search_helltide.lua
+++ tasks/search_helltide.lua
@@ -2,6 +2,7 @@ local utils = require "core.utils"
 local tracker = require "core.tracker"
 local helltide_task = require "tasks.helltide"
 local enums = require "data.enums"
+local plugin_label = "helltide_revamped"
 
 local current_city_index = 0
 
@@ -42,6 +43,9 @@ local search_helltide_task = {
         if not utils.helltide_active() then
             console.print("Helltide is not active, wait until helltide starts")
             if not utils.player_in_zone("Scos_Cerrigar") then
+                if PLUGIN_alfred_the_butler then
+                    PLUGIN_alfred_the_butler.trigger_tasks(plugin_label,function()end)
+                end
                 teleport_to_waypoint(0x76D58) -- Go to cerrigar and wait for helltide
             end
             return
