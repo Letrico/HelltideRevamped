75c56266df8a7c028f886860e221d4e07b41867d
Letrico <15125831+Letrico@users.noreply.github.com>
2025-05-17 00:40:08 +1000

    movement and interact is doing dumb stuff

--- tasks/helltide.lua
+++ tasks/helltide.lua
@@ -383,6 +383,7 @@ local helltide_task = {
                 explorerlite:set_custom_target(found_ore:get_position())
                 explorerlite:move_to_target()
                 -- pathfinder.force_move(found_ore:get_position())
+                return
             end
             interact_object(found_ore)
         else
@@ -404,6 +405,7 @@ local helltide_task = {
                 explorerlite:set_custom_target(found_herb:get_position())
                 explorerlite:move_to_target()
                 -- pathfinder.force_move(found_herb:get_position())
+                return
             end
             interact_object(found_herb)
         else
